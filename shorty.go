58ffd0666d4191ca58bc408ca814e433d2a9ceff
João Neves <sevenjp@gmail.com>
2013-11-06 11:28:36 +0100

    Uglified tries to add pattern substitutions. Clean up imminent. *whiirrrrr*

--- shorty.go
+++ shorty.go
@@ -5,13 +5,36 @@ import (
 	"net/http"
 )
 
+type TrieNode interface {
+	IsPattern() bool
+	PatternName() *string
+	Value() interface{}
+	Lookup(r rune) *TrieNode
+	Find(s string) *Match
+	Insert(key string, value interface{})
+}
+
+type Match struct {
+	value interface{}
+	matches map[string]string
+}
+
 type Trie struct {
+	pattern_name *string
 	value interface{}
 	children map[rune]*Trie
 }
 
 func CreateTrie() *Trie {
 	return &Trie{value: nil, children: make(map[rune]*Trie)}
+}
+
+func (t Trie) IsPattern() bool {
+	return t.pattern_name != nil
+}
+
+func (t Trie) PatternName() *string {
+	return t.pattern_name
 }
 
 func (t Trie) Lookup(r rune) *Trie {
@@ -21,29 +44,72 @@ func (t Trie) Lookup(r rune) *Trie {
 	return nil
 }
 
-func (t Trie) Find(s string) interface{} {
+func (t Trie) Find(s string) *Match {
+	matched_patterns := make(map[string]string)
+	var current_pattern *string
+	var current_value []rune
 	node := &t
 	for _, r := range s {
 		n := node.Lookup(r)
 		if n != nil {
 			node = n
+			if current_pattern != nil {
+				matched_patterns[*current_pattern] = string(current_value)
+				current_pattern = nil
+			}
+		} else if current_pattern != nil {
+			current_value = append(current_value, r)
 		} else {
-			return nil
+			for key := range node.children {
+				child := node.children[key]
+				if child.IsPattern() {
+					current_pattern = child.PatternName()
+					current_value = make([]rune, 10, 255)
+					current_value = append(current_value, r)
+					node = child
+					break
+				}
+			}
+			if current_pattern != nil {
+				continue
+			}
+			return &Match{nil, nil}
 		}
 	}
-	return node.value
+	if current_pattern != nil {
+		matched_patterns[*current_pattern] = string(current_value)
+	}
+	return &Match{node.value, matched_patterns}
 }
 
 func (t Trie) Insert(key string, value interface{}) {
+	in_pattern := false
+	var last_rune rune
+	var pattern_name []rune
 	node := &t
 	for _, r := range key {
-		n := node.Lookup(r)
-		if n != nil {
-			node = n
-		} else {
+		if r == '{' && !in_pattern {
+			in_pattern = true
+			pattern_name = make([]rune, 15)
+		} else if r != '}' && in_pattern {
+			pattern_name = append(pattern_name, r)
+		} else if r == '}' && in_pattern {
+			in_pattern = false
 			new_node := CreateTrie()
-			node.children[r] = new_node
+			pattern_name_str := string(pattern_name)
+			new_node.pattern_name = &pattern_name_str
+			node.children[last_rune] = new_node
 			node = new_node
+		} else {
+			n := node.Lookup(r)
+			if n != nil {
+				node = n
+			} else {
+				new_node := CreateTrie()
+				node.children[r] = new_node
+				node = new_node
+			}
+			last_rune = r
 		}
 	}
 	node.value = value
@@ -51,7 +117,11 @@ func (t Trie) Insert(key string, value interface{}) {
 
 func (t Trie) Print() {
 	for key, _ := range t.children {
-		fmt.Printf("%#U - %T %s\n", key, t.children[key].value, t.children[key].children)
+		if t.children[key].IsPattern() {
+			fmt.Printf("{%+v} - %T %s\n", *(t.children[key].PatternName()), t.children[key].value, t.children[key].children)
+		} else {
+			fmt.Printf("%#U - %T %s\n", key, t.children[key].value, t.children[key].children)
+		}
 		t.children[key].Print()
 	}
 }
@@ -67,19 +137,19 @@ type Router interface {
 	Route(writer http.ResponseWriter, req *http.Request)
 }
 
-type SimpleRouter struct {
+type StaticRouter struct {
 	routes map[string]Resource
 }
 
-func (r SimpleRouter) AddRoute(route string, resource Resource) {
+func (r StaticRouter) AddRoute(route string, resource Resource) {
 	r.routes[route] = resource
 }
 
-func (r SimpleRouter) RemoveRoute(route string) {
+func (r StaticRouter) RemoveRoute(route string) {
 	delete(r.routes, route)
 }
 
-func (r SimpleRouter) Route(writer http.ResponseWriter, request *http.Request) {
+func (r StaticRouter) Route(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println(request.Method)
 	method := request.Method
 	url := request.URL.String()
@@ -114,7 +184,8 @@ func (r MatchingRouter) RemoveRoute(route string) {
 func (r MatchingRouter) Route(writer http.ResponseWriter, request *http.Request) {
 	method := request.Method
 	path := request.URL.Path
-	if resource, ok := r.trie.Find(path).(Resource); ok {
+	match := r.trie.Find(path)
+	if resource, ok := match.value.(Resource); ok {
 		switch method {
 		case "GET":
 			fmt.Fprintf(writer, resource.Get(path))
@@ -161,10 +232,22 @@ func (r ShortyResource) Post(url string, data interface{}) string {
 	return "shorty post"
 }
 
+type FooResource struct {}
+
+func (r FooResource) Get(url string) string {
+	return "foo"
+}
+
+func (r FooResource) Post(url string, data interface{}) string {
+	return "foo post"
+}
+
 func main() {
-	//router := SimpleRouter{routes: make(map[string]Resource)}
+	//router := StaticRouter{routes: make(map[string]Resource)}
 	router := MatchingRouter{trie: CreateTrie()}
 	router.AddRoute("/", new(ShortyResource))
+	router.AddRoute("/{id}", new(ShortyResource))
+	router.AddRoute("/foo/{foo}/{bar}/baz", new(FooResource))
 	router.trie.Print()
 	rh := MakeRoutingHandler()
 	rh.SetRouter(router)
