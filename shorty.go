8bcfdbeca0bd8163e3b6f834c02e0951b4fe322d
João Neves <sevenjp@gmail.com>
2013-12-09 16:28:49 +0100

    Refactor (read: break) everything to make use of microbe

--- shorty.go
+++ shorty.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"github.com/jpgneves/microbe/routers"
-	"log"
-	"net"
-	"net/http"
-	"strconv"
+	"github.com/jpgneves/microbe"
 )
 
 func main() {
-	router := routers.NewMatchingRouter()
-	config := ReadConfig("./shorty.config")
-	router.AddRoute("/", &SiteResource{config})
+	microbe := microbe.Init("./shorty.config")
+	microbe.AddRoute("/", &SiteResource{config})
 	shorty := NewShortyResource(config)
-	router.AddRoute("/:id", shorty)
-	router.AddRoute("/create", shorty)
-	rh := routers.MakeRoutingHandler(router)
-	addr := net.JoinHostPort(*config.ListenAddr.Address, strconv.Itoa(config.ListenAddr.Port))
-	log.Printf("Starting server on %s", addr)
-	http.ListenAndServe(addr, rh)
+	microbe.AddRoute("/:id", shorty)
+	microbe.AddRoute("/create", shorty)
+	microbe.Start(false)
 }
